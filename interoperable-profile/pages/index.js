7f9b9a18dffcdd5fcc434657c752a23e739b11b9
ambiti0n <fabulousfresh010@gmail.com>
2022-03-17 12:40:34 +0100

    useViewerConnection to connect and disconnect ceramic wallet added

--- interoperable-profile/pages/index.js
+++ interoperable-profile/pages/index.js
@@ -2,6 +2,9 @@ import { Web3Provider } from "@ethersproject/providers";
 
 import { useEffect, useRef, useState } from "react";
 import Web3Modal from "web3modal";
+import { useViewerConnection } from "@self.id/react";
+
+const [connection, connect, disconnect] = useViewerConnection();
 
 const web3ModalRef = useRef();
 
