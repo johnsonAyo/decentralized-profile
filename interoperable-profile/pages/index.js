9c6b8ae2fac14e9a418a9fa09bd6128f15aa419f
ambiti0n <fabulousfresh010@gmail.com>
2022-03-17 12:43:21 +0100

    EthereumAuthProvider is used to connect your Ethereum wallet to our 3ID

--- interoperable-profile/pages/index.js
+++ interoperable-profile/pages/index.js
@@ -4,6 +4,8 @@ import { useEffect, useRef, useState } from "react";
 import Web3Modal from "web3modal";
 import { useViewerConnection } from "@self.id/react";
 
+import { EthereumAuthProvider } from "@self.id/web";
+
 const [connection, connect, disconnect] = useViewerConnection();
 
 const web3ModalRef = useRef();
@@ -12,4 +14,26 @@ const getProvider = async () => {
   const provider = await web3ModalRef.current.connect();
   const wrappedProvider = new Web3Provider(provider);
   return wrappedProvider;
+};
+
+useEffect(() => {
+  if (connection.status !== "connected") {
+    web3ModalRef.current = new Web3Modal({
+      network: "rinkeby",
+      providerOptions: {},
+      disableInjectedProvider: false,
+    });
+  }
+}, [connection.status]);
+
+const connectToSelfID = async () => {
+  const ethereumAuthProvider = await getEthereumAuthProvider();
+  connect(ethereumAuthProvider);
+};
+
+const getEthereumAuthProvider = async () => {
+  const wrappedProvider = await getProvider();
+  const signer = wrappedProvider.getSigner();
+  const address = await signer.getAddress();
+  return new EthereumAuthProvider(wrappedProvider.provider, address);
 };
